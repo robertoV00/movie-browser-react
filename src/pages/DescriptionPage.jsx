9c4299836a48bc47a72d1c58e5f1a16ed23fd970
Roberto Vega <droz.rv@gmail.com>
2025-04-30 02:00:42 -0400

    searchpage responsiveness added

--- src/pages/DescriptionPage.jsx
+++ src/pages/DescriptionPage.jsx
@@ -7,6 +7,8 @@ import { faPlay } from '@fortawesome/free-solid-svg-icons';
 import Movie from '../ui/Movie';
 import BottomHalf from '../ui/BottomHalf';
 import Sidebar from '../Sidebar';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 
 const DescriptionPage = () => {
@@ -39,31 +41,59 @@ const DescriptionPage = () => {
 
     return (
         <>
-        <div className="layout">
-            <Sidebar />
-            <div className="main-content">
-                <div className='master-container'>
-                    <div className="top-container">
-                        <div className="image-box-container">
-                            <img className="movie-image" src={movie.Poster} />
+            <div className="layout">
+                <Sidebar />
+                <div className="main-content">
+                    <div className='master-container'>
+                        <div className="top-container">
+                            <div className="image-box-container">
+                                {loading ? (
+                                    <Skeleton height={400} width={300} />
+                                ) : (
+                                    <img className="movie-image" src={movie.Poster} alt={movie.Title} />
+                                )}
+                            </div>
+                            <div className="description-box-container">
+                                {loading ? (
+                                    <>
+                                        <Skeleton height={40} width={300} />
+                                        <Skeleton height={20} width={200} style={{ marginTop: '16px' }} />
+                                        <Skeleton height={24} width={100} style={{ marginTop: '24px' }} />
+                                        <Skeleton height={100} style={{ marginTop: '16px' }} />
+                                        <Skeleton height={40} width={120} style={{ marginTop: '24px' }} />
+                                    </>
+                                ) : (
+                                    <>
+                                        <h1 className='description-movie-title'>{movie.Title}</h1>
+                                        <p className='description-information'>
+                                            <span className="description-date description">{movie.Released}</span> . 
+                                            <span className="description-movie-length description">{movie.Runtime}</span> . 
+                                            <span className="description-rating description">{movie.imdbRating}/10</span>
+                                        </p>
+                                        <h3 className="overview-title">Overview: </h3>
+                                        <p className="description-overview">{movie.Plot}</p>
+                                        <button disabled className="description-watch-btn">
+                                            <FontAwesomeIcon className="play-button" icon={faPlay} /> Watch
+                                        </button>
+                                    </>
+                                )}
+                            </div>
                         </div>
-                        <div className="description-box-container">
-                            <h1 className='description-movie-title'>{movie.Title}</h1>
-                            <p className='description-information'><span className="description-date description">{movie.Released}</span> . <span className="description-movie-length description">{movie.Runtime}</span> . <span className="description-rating description">{movie.imdbRating}/10</span></p>
-                            <h3 className="overview-title">Overview: </h3>
-                            <p className="description-overview">{movie.Plot}</p>
-                            <button disabled className="description-watch-btn">
-                            <FontAwesomeIcon className="play-button" icon={faPlay} /> Watch
-                            </button>
+                        <div className="description-bottom-container">
+                            <h2 className="bottom-container-title">Recommended Movies</h2>
+                            {loading ? (
+                                <div style={{ display: 'flex', gap: '20px' }}>
+                                    {[...Array(4)].map((_, index) => (
+                                        <Skeleton key={index} height={300} width={200} />
+                                    ))}
+                                </div>
+                            ) : (
+                                <BottomHalf />
+                            )}
                         </div>
-                    </div>
-                    <div className="description-bottom-container">
-                        <h2 className="bottom-container-title">Recommended Movies</h2>
-                        <BottomHalf />
                     </div>
                 </div>
             </div>
-        </div>
         </>
     );
 }
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -14,6 +14,15 @@ const SearchPage = () => {
     const [movies, setMovies] = useState([]);
     const [loading, setLoading] = useState(false)
 
+    const handleSearch = async () => {
+        setLoading(true);
+        try {
+            await fetchMovies(); // your existing fetch function
+        } finally {
+            setLoading(false);
+        }
+    };
+
 
     const [searchParams] = useSearchParams();
     const searchValue = searchParams.get('search');
@@ -51,8 +60,10 @@ const SearchPage = () => {
                                 Search for a movie
                             </h1>
                         <div className="search-box-container">
-                            <button  className="search-button">
-                            <FontAwesomeIcon className='search-glass' icon={faMagnifyingGlass} />
+                            <button  
+                            onClick={handleSearch}
+                            className="search-button">
+                                <FontAwesomeIcon className='search-glass' icon={faMagnifyingGlass} />
                             </button>
                         </div>
                         </div>
@@ -62,9 +73,14 @@ const SearchPage = () => {
                     <div className="row movies-items-row">
                         <div className="searchpage-movies-list">
                             {loading ? (
-                                    <div className="loading-spinner">
-                                        <FontAwesomeIcon color="red" icon={faSpinner} className="fa-spin" />
-                                    </div>
+                                        <svg 
+                                            className="loading-spinner fa-spin"
+                                            xmlns="http://www.w3.org/2000/svg" 
+                                            viewBox="0 0 512 512"
+                                            fill="crimson"  // Add this line
+                                        >
+                                            <path d="M386.3 160L336 160c-17.7 0-32 14.3-32 32s14.3 32 32 32l128 0c17.7 0 32-14.3 32-32l0-128c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 51.2L414.4 97.6c-87.5-87.5-229.3-87.5-316.8 0s-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3s163.8-62.5 226.3 0L386.3 160z"/>
+                                        </svg>
                                 ) : movies.length > 0 ? (
                                     movies.slice(0, 6).map((movie) => (
                                         <DynamicMovies movie={movie} key={movie.imdbID}/>
